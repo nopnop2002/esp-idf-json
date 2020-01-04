028b982ee254bbfc40dd089dbb72b39aefa50891
nopnop2002 <hau26280@tree.odn.ne.jp>
2020-01-04 21:48:44 +0900

    First Release

--- json-subarrays/main/main.c
+++ json-subarrays/main/main.c
@@ -14,16 +14,16 @@
 static const char *TAG = "JSON";
 
 char *JSON_Types(int type) {
-    if (type == cJSON_Invalid) return ("cJSON_Invalid");
-    if (type == cJSON_False) return ("cJSON_False");
-    if (type == cJSON_True) return ("cJSON_True");
-    if (type == cJSON_NULL) return ("cJSON_NULL");
-    if (type == cJSON_Number) return ("cJSON_Number");
-    if (type == cJSON_String) return ("cJSON_String");
-    if (type == cJSON_Array) return ("cJSON_Array");
-    if (type == cJSON_Object) return ("cJSON_Object");
-    if (type == cJSON_Raw) return ("cJSON_Raw");
-    return NULL;
+	if (type == cJSON_Invalid) return ("cJSON_Invalid");
+	if (type == cJSON_False) return ("cJSON_False");
+	if (type == cJSON_True) return ("cJSON_True");
+	if (type == cJSON_NULL) return ("cJSON_NULL");
+	if (type == cJSON_Number) return ("cJSON_Number");
+	if (type == cJSON_String) return ("cJSON_String");
+	if (type == cJSON_Array) return ("cJSON_Array");
+	if (type == cJSON_Object) return ("cJSON_Object");
+	if (type == cJSON_Raw) return ("cJSON_Raw");
+	return NULL;
 }
 
 void app_main()
@@ -82,35 +82,35 @@ void app_main()
 	ESP_LOGI(TAG, "resolutions2->type=%s", JSON_Types(resolutions2->type));
 	int resolutions2_array_size = cJSON_GetArraySize(resolutions2);
 	ESP_LOGI(TAG, "resolutions2_array_size=%d", resolutions2_array_size);
-    for (int i=0;i<resolutions2_array_size;i++) {
-        cJSON *array = cJSON_GetArrayItem(resolutions2,i);
-        //ESP_LOGI(TAG, "array->type=%s", JSON_Types(array->type));
-        int width = cJSON_GetObjectItem(array,"width")->valueint;
-        int height = cJSON_GetObjectItem(array,"height")->valueint;
-        ESP_LOGI(TAG, "width=%d",width);
-        ESP_LOGI(TAG, "height=%d",height);
-    }
+	for (int i=0;i<resolutions2_array_size;i++) {
+		cJSON *array = cJSON_GetArrayItem(resolutions2,i);
+		//ESP_LOGI(TAG, "array->type=%s", JSON_Types(array->type));
+		int width = cJSON_GetObjectItem(array,"width")->valueint;
+		int height = cJSON_GetObjectItem(array,"height")->valueint;
+		ESP_LOGI(TAG, "width=%d",width);
+		ESP_LOGI(TAG, "height=%d",height);
+	}
 
 	cJSON *resolutionsWidth2 = cJSON_GetObjectItem(root2,"resolutionsWidth");
 	ESP_LOGI(TAG, "resolutionsWidth2->type=%s", JSON_Types(resolutionsWidth2->type));
 	int resolutionsWidth2_array_size = cJSON_GetArraySize(resolutionsWidth2);
 	ESP_LOGI(TAG, "resolutionsWidth2_array_size=%d", resolutionsWidth2_array_size);
-    for (int i=0;i<resolutionsWidth2_array_size;i++) {
-        cJSON *array = cJSON_GetArrayItem(resolutionsWidth2,i);
-        //ESP_LOGI(TAG, "array->type=%s", JSON_Types(array->type));
-        int resolutionsWidth = array->valueint;
-        ESP_LOGI(TAG, "resolutionsWidth[%d]=%d",i, resolutionsWidth);
+	for (int i=0;i<resolutionsWidth2_array_size;i++) {
+		cJSON *array = cJSON_GetArrayItem(resolutionsWidth2,i);
+		//ESP_LOGI(TAG, "array->type=%s", JSON_Types(array->type));
+		int resolutionsWidth = array->valueint;
+		ESP_LOGI(TAG, "resolutionsWidth[%d]=%d",i, resolutionsWidth);
 	}
 
 	cJSON *resolutionsHeight2 = cJSON_GetObjectItem(root2,"resolutionsHeight");
 	ESP_LOGI(TAG, "resolutionsHeight2->type=%s", JSON_Types(resolutionsHeight2->type));
 	int resolutionsHeight2_array_size = cJSON_GetArraySize(resolutionsHeight2);
 	ESP_LOGI(TAG, "resolutionsHeight2_array_size=%d", resolutionsHeight2_array_size);
-    for (int i=0;i<resolutionsHeight2_array_size;i++) {
-        cJSON *array = cJSON_GetArrayItem(resolutionsHeight2,i);
-        //ESP_LOGI(TAG, "array->type=%s", JSON_Types(array->type));
-        int resolutionsHeight = array->valueint;
-        ESP_LOGI(TAG, "resolutionsHeight[%d]=%d",i, resolutionsHeight);
+	for (int i=0;i<resolutionsHeight2_array_size;i++) {
+		cJSON *array = cJSON_GetArrayItem(resolutionsHeight2,i);
+		//ESP_LOGI(TAG, "array->type=%s", JSON_Types(array->type));
+		int resolutionsHeight = array->valueint;
+		ESP_LOGI(TAG, "resolutionsHeight[%d]=%d",i, resolutionsHeight);
 	}
 	cJSON_Delete(root2);
 }
