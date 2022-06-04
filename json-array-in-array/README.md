8ea267ecab8b67608c4fbf488ecb4d19c2ac53cb
nopnop2002 <hau26280@tree.odn.ne.jp>
2022-06-04 16:28:00 +0900

    Added json-array-in-array

---
+++ json-array-in-array/README.md
@@ -0,0 +1,40 @@
+# JSON Example
+
+## Serialize
+```
+[["abc", 120], ["abc", 121], ["abc", 122], ["abc", 123], ["abc", 124], ["abc", 125], ["abc", 126], ["abc", 127], ["abc", 128], ["abc", 129]]
+```
+
+## DeSerialize
+```
+I (365) JSON: cJSON_Array
+I (365) JSON: cJSON_String string=abc
+I (365) JSON: cJSON_Number int=120 double=120.000000
+I (375) JSON: cJSON_Array
+I (375) JSON: cJSON_String string=abc
+I (385) JSON: cJSON_Number int=121 double=121.000000
+I (385) JSON: cJSON_Array
+I (395) JSON: cJSON_String string=abc
+I (395) JSON: cJSON_Number int=122 double=122.000000
+I (405) JSON: cJSON_Array
+I (405) JSON: cJSON_String string=abc
+I (405) JSON: cJSON_Number int=123 double=123.000000
+I (415) JSON: cJSON_Array
+I (415) JSON: cJSON_String string=abc
+I (425) JSON: cJSON_Number int=124 double=124.000000
+I (425) JSON: cJSON_Array
+I (435) JSON: cJSON_String string=abc
+I (435) JSON: cJSON_Number int=125 double=125.000000
+I (445) JSON: cJSON_Array
+I (445) JSON: cJSON_String string=abc
+I (445) JSON: cJSON_Number int=126 double=126.000000
+I (455) JSON: cJSON_Array
+I (455) JSON: cJSON_String string=abc
+I (465) JSON: cJSON_Number int=127 double=127.000000
+I (465) JSON: cJSON_Array
+I (475) JSON: cJSON_String string=abc
+I (475) JSON: cJSON_Number int=128 double=128.000000
+I (485) JSON: cJSON_Array
+I (485) JSON: cJSON_String string=abc
+I (485) JSON: cJSON_Number int=129 double=129.000000
+```
---
+++ json-array-in-array/main/main.c
@@ -0,0 +1,75 @@
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_chip_info.h"
+#include "cJSON.h"
+
+static const char *TAG = "JSON";
+
+void JSON_Print(cJSON *element) {
+	if (element->type == cJSON_Invalid) ESP_LOGI(TAG, "cJSON_Invalid");
+	if (element->type == cJSON_False) ESP_LOGI(TAG, "cJSON_False");
+	if (element->type == cJSON_True) ESP_LOGI(TAG, "cJSON_True");
+	if (element->type == cJSON_NULL) ESP_LOGI(TAG, "cJSON_NULL");
+	if (element->type == cJSON_Number) ESP_LOGI(TAG, "cJSON_Number int=%d double=%f", element->valueint, element->valuedouble);
+	if (element->type == cJSON_String) ESP_LOGI(TAG, "cJSON_String string=%s", element->valuestring);
+	if (element->type == cJSON_Array) {
+		ESP_LOGI(TAG, "cJSON_Array");
+		cJSON *child_element = NULL;
+		cJSON_ArrayForEach(child_element, element) {
+			JSON_Print(child_element);
+		}
+	}
+	if (element->type == cJSON_Object) {
+		ESP_LOGI(TAG, "cJSON_Object");
+		cJSON *child_element = NULL;
+		cJSON_ArrayForEach(child_element, element) {
+			JSON_Print(child_element);
+		}
+	}
+	if (element->type == cJSON_Raw) ESP_LOGI(TAG, "cJSON_Raw");
+}
+
+
+void app_main()
+{
+	ESP_LOGI(TAG, "Serialize.....");
+	cJSON *root;
+	root = cJSON_CreateArray();
+
+	cJSON *array[10];
+	cJSON *element;
+
+	int array_num = 10;
+	for (int i=0;i<array_num;i++) {
+		array[i] = cJSON_CreateArray();
+		element = cJSON_CreateString("abc");
+		cJSON_AddItemToArray(array[i], element);
+		element = cJSON_CreateNumber(120+i);
+		cJSON_AddItemToArray(array[i], element);
+		cJSON_AddItemToArray(root, array[i]);
+		//cJSON_Delete(array);
+	}
+	char *my_json_string = cJSON_Print(root);
+	ESP_LOGI(TAG, "my_json_string\n%s",my_json_string);
+	cJSON_Delete(root);
+
+
+	ESP_LOGI(TAG, "Deserialize.....");
+	cJSON *root2 = cJSON_Parse(my_json_string);
+	int root2_array_size = cJSON_GetArraySize(root2); 
+	ESP_LOGI(TAG, "root2_array_size=%d", root2_array_size);
+	for (int i=0;i<root2_array_size;i++) {
+		cJSON *element2 = cJSON_GetArrayItem(root2, i);
+		JSON_Print(element2);
+	}
+	cJSON_Delete(root2);
+
+	// Buffers returned by cJSON_Print must be freed by the caller.
+	// Please use the proper API (cJSON_free) rather than directly calling stdlib free.
+	cJSON_free(my_json_string);
+}
