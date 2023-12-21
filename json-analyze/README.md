cec540c66ba17b9375e19dee53d7cc7112bce656
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-12-21 10:07:08 +0900

    Updated JSON_Analyze function

--- json-analyze/README.md
+++ json-analyze/README.md
@@ -3,28 +3,76 @@
 ## Serialize
 ```
 {
-        "version":      "v4.1-dev-1572-g30372f5a4",
+        "version":      "v5.1.2-dirty",
         "cores":        2,
-        "true": true,
-        "false":        false,
+        "wifi": true,
+        "ethernet":     false,
         "bool": false,
-        "null": null
+        "null": null,
+        "screen":       {
+                "type": "rect",
+                "width":        1920,
+                "height":       1080,
+                "interlace":    false,
+                "luster":       true
+        },
+        "screen":       ["rect", 1920, 1080, false, true]
 }
 ```
 
 ## DeSerialize
 ```
-I (314) JSON: Deserialize.....
-I (314) JSON: [version]
-I (324) JSON: v4.0-dev-1186-gf16f4297c-dirty
-I (324) JSON: [cores]
-I (324) JSON: int=2 double=2.000000
-I (334) JSON: [true]
-I (334) JSON: True
-I (334) JSON: [false]
-I (344) JSON: False
-I (344) JSON: [bool]
-I (344) JSON: False
-I (344) JSON: [null]
-I (354) JSON: Null
+I (381) JSON: Deserialize.....
+I (381) JSON: [version]
+I (391) JSON: v5.1.2-dirty
+I (391) JSON: [cores]
+I (391) JSON: int=2 double=2.000000
+I (401) JSON: [wifi]
+I (401) JSON: True
+I (401) JSON: [ethernet]
+I (411) JSON: False
+I (411) JSON: [bool]
+I (411) JSON: False
+I (411) JSON: [null]
+I (421) JSON: Null
+I (421) JSON: [screen]
+I (421) JSON: [type]
+I (431) JSON: rect
+I (431) JSON: [width]
+I (431) JSON: int=1920 double=1920.000000
+I (441) JSON: [height]
+I (441) JSON: int=1080 double=1080.000000
+I (441) JSON: [interlace]
+I (451) JSON: False
+I (451) JSON: [luster]
+I (451) JSON: True
+I (461) JSON: [screen]
+I (461) JSON: rect
+I (461) JSON: int=1920 double=1920.000000
+I (471) JSON: int=1080 double=1080.000000
+I (471) JSON: False
+I (471) JSON: True
+```
+
+## Analysis
+```
+elementsLength=18
+element[0] cJSON_String [version] [v5.1.2-dirty]
+element[1] cJSON_Number [cores] int=2 double=2.000000
+element[2] cJSON_True [wifi]
+element[3] cJSON_False [ethernet]
+element[4] cJSON_False [bool]
+element[5] cJSON_NULL [null]
+element[6] cJSON_Object [screen]
+element[7] cJSON_String [type] [rect]
+element[8] cJSON_Number [width] int=1920 double=1920.000000
+element[9] cJSON_Number [height] int=1080 double=1080.000000
+element[10] cJSON_False [interlace]
+element[11] cJSON_True [luster]
+element[12] cJSON_Array [screen]
+element[13] cJSON_String [array item] [rect]
+element[14] cJSON_Number [array item] int=1920 double=1920.000000
+element[15] cJSON_Number [array item] int=1080 double=1080.000000
+element[16] cJSON_False [array item]
+element[17] cJSON_True [array item]
 ```
--- json-analyze/main/main.c
+++ json-analyze/main/main.c
@@ -2,6 +2,9 @@
  *
  * This sample code is in the public domain.
  */
+
+#include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
@@ -9,6 +12,14 @@
 #include "cJSON.h"
 
 static const char *TAG = "JSON";
+
+typedef struct {
+	int type;
+	char name[32];
+	int valueint;
+	double valuedouble;
+	char valuestring[32];
+} JSON_t;
 
 char *JSON_Types(int type) {
 	if (type == cJSON_Invalid) return ("cJSON_Invalid");
@@ -24,7 +35,7 @@ char *JSON_Types(int type) {
 }
 
 
-void JSON_Analyze(const cJSON * const root) {
+esp_err_t JSON_Analyze(const cJSON * const root, JSON_t **elements, size_t *elementsLength) {
 	//ESP_LOGI(TAG, "root->type=%s", JSON_Types(root->type));
 	cJSON *current_element = NULL;
 	//ESP_LOGI(TAG, "roo->child=%p", root->child);
@@ -32,35 +43,63 @@ void JSON_Analyze(const cJSON * const root) {
 	cJSON_ArrayForEach(current_element, root) {
 		//ESP_LOGI(TAG, "type=%s", JSON_Types(current_element->type));
 		//ESP_LOGI(TAG, "current_element->string=%p", current_element->string);
+
+		if (*elements == NULL) {
+			(*elementsLength)++;
+			*elements = (JSON_t *)malloc(sizeof(JSON_t));
+		} else {
+			(*elementsLength)++;
+			*elements = (JSON_t *)realloc(*elements, *elementsLength * sizeof(JSON_t));
+		}
+		if (*elements == NULL) {
+			ESP_LOGE(TAG, "Error allocating memory for elements");
+			return ESP_ERR_NO_MEM;
+		}
+
 		if (current_element->string) {
-			const char* string = current_element->string;
-			ESP_LOGI(TAG, "[%s]", string);
+			const char* name = current_element->string;
+			ESP_LOGI(TAG, "[%s]", name);
+			strncpy((*elements+(*elementsLength)-1)->name, name, 31);
+		} else {
+			strcpy((*elements+(*elementsLength)-1)->name, "array item");
 		}
 		if (cJSON_IsInvalid(current_element)) {
 			ESP_LOGI(TAG, "Invalid");
+			(*elements+(*elementsLength)-1)->type = cJSON_Invalid;
 		} else if (cJSON_IsFalse(current_element)) {
 			ESP_LOGI(TAG, "False");
+			(*elements+(*elementsLength)-1)->type = cJSON_False;
 		} else if (cJSON_IsTrue(current_element)) {
 			ESP_LOGI(TAG, "True");
+			(*elements+(*elementsLength)-1)->type = cJSON_True;
 		} else if (cJSON_IsNull(current_element)) {
 			ESP_LOGI(TAG, "Null");
+			(*elements+(*elementsLength)-1)->type = cJSON_NULL;
 		} else if (cJSON_IsNumber(current_element)) {
 			int valueint = current_element->valueint;
 			double valuedouble = current_element->valuedouble;
 			ESP_LOGI(TAG, "int=%d double=%f", valueint, valuedouble);
+			(*elements+(*elementsLength)-1)->type = cJSON_Number;
+			(*elements+(*elementsLength)-1)->valueint = valueint;
+			(*elements+(*elementsLength)-1)->valuedouble = valuedouble;
 		} else if (cJSON_IsString(current_element)) {
 			const char* valuestring = current_element->valuestring;
 			ESP_LOGI(TAG, "%s", valuestring);
+			(*elements+(*elementsLength)-1)->type = cJSON_String;
+			strncpy((*elements+(*elementsLength)-1)->valuestring, valuestring, 31);
 		} else if (cJSON_IsArray(current_element)) {
-			//ESP_LOGI(TAG, "Array");
-			JSON_Analyze(current_element);
+			ESP_LOGD(TAG, "Array");
+			(*elements+(*elementsLength)-1)->type = cJSON_Array;
+			JSON_Analyze(current_element, elements, elementsLength);
 		} else if (cJSON_IsObject(current_element)) {
-			//ESP_LOGI(TAG, "Object");
-			JSON_Analyze(current_element);
+			ESP_LOGD(TAG, "Object");
+			(*elements+(*elementsLength)-1)->type = cJSON_Object;
+			JSON_Analyze(current_element, elements, elementsLength);
 		} else if (cJSON_IsRaw(current_element)) {
 			ESP_LOGI(TAG, "Raw(Not support)");
 		}
 	}
+	return ESP_OK;
 }
 
 
@@ -73,21 +112,83 @@ void app_main()
 	esp_chip_info(&chip_info);
 	cJSON_AddStringToObject(root, "version", IDF_VER);
 	cJSON_AddNumberToObject(root, "cores", chip_info.cores);
-	cJSON_AddTrueToObject(root, "true");
-	cJSON_AddFalseToObject(root, "false");
+	cJSON_AddTrueToObject(root, "wifi");
+	cJSON_AddFalseToObject(root, "ethernet");
 	cJSON_AddBoolToObject(root, "bool", 0);
 	cJSON_AddNullToObject(root, "null");
+
+	// Create object
+	cJSON *object;
+	cJSON_AddItemToObject(root, "screen", object=cJSON_CreateObject());
+	cJSON_AddStringToObject(object,"type", "rect");
+	cJSON_AddNumberToObject(object,"width", 1920);
+	cJSON_AddNumberToObject(object,"height", 1080);
+	cJSON_AddFalseToObject (object,"interlace");
+	cJSON_AddTrueToObject(object,"luster");
+
+	// Create array
+	cJSON *array;
+	array = cJSON_AddArrayToObject(root, "screen");
+	cJSON *array_element;
+	array_element = cJSON_CreateString("rect");
+	cJSON_AddItemToArray(array, array_element);
+	array_element = cJSON_CreateNumber(1920);
+	cJSON_AddItemToArray(array, array_element);
+	array_element = cJSON_CreateNumber(1080);
+	cJSON_AddItemToArray(array, array_element);
+	array_element = cJSON_CreateFalse();
+	cJSON_AddItemToArray(array, array_element);
+	array_element = cJSON_CreateTrue();
+	cJSON_AddItemToArray(array, array_element);
+
 	//const char *my_json_string = cJSON_Print(root);
 	char *my_json_string = cJSON_Print(root);
 	ESP_LOGI(TAG, "my_json_string\n%s",my_json_string);
 	cJSON_Delete(root);
 
 	ESP_LOGI(TAG, "Deserialize.....");
+	JSON_t *elements = NULL;
+	size_t elementsLength = 0;
 	cJSON *root2 = cJSON_Parse(my_json_string);
-	JSON_Analyze(root2);
+	JSON_Analyze(root2, &elements, &elementsLength);
 	cJSON_Delete(root2);
 
 	// Buffers returned by cJSON_Print must be freed by the caller.
 	// Please use the proper API (cJSON_free) rather than directly calling stdlib free.
 	cJSON_free(my_json_string);
+
+	printf("elementsLength=%d\n", elementsLength);
+	for (int i=0;i<elementsLength;i++) {
+		printf("element[%d]", i);
+		if ((elements+i)->type == cJSON_Invalid) {
+			printf(" cJSON_Invalid");
+		} else if ((elements+i)->type == cJSON_False) {
+			printf(" cJSON_False");
+			printf(" [%s]", (elements+i)->name);
+		} else if ((elements+i)->type == cJSON_True) {
+			printf(" cJSON_True");
+			printf(" [%s]", (elements+i)->name);
+		} else if ((elements+i)->type == cJSON_NULL) {
+			printf(" cJSON_NULL");
+			printf(" [%s]", (elements+i)->name);
+		} else if ((elements+i)->type == cJSON_Number) {
+			printf(" cJSON_Number");
+			printf(" [%s]", (elements+i)->name);
+			printf(" int=%d", (elements+i)->valueint);
+			printf(" double=%lf", (elements+i)->valuedouble);
+		} else if ((elements+i)->type == cJSON_String) {
+			printf(" cJSON_String");
+			printf(" [%s]", (elements+i)->name);
+			printf(" [%s]", (elements+i)->valuestring);
+		} else if ((elements+i)->type == cJSON_Array) {
+			printf(" cJSON_Array");
+			printf(" [%s]", (elements+i)->name);
+		} else if ((elements+i)->type == cJSON_Object) {
+			printf(" cJSON_Object");
+			printf(" [%s]", (elements+i)->name);
+		}
+		printf("\n");
+	}
+
+	free(elements);
 }
