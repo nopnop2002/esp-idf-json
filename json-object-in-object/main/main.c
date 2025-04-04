3eebe9265558e875c65ab86b9fae8ccf6a293bc9
nopnop2002 <>
2025-04-04 20:17:28 +0900

    Remove tabs in code

--- json-object-in-object/main/main.c
+++ json-object-in-object/main/main.c
@@ -12,15 +12,16 @@ static const char *TAG = "JSON";
 void app_main()
 {
 	ESP_LOGI(TAG, "Serialize.....");
-	cJSON *root, *fmt;
+	cJSON *root, *format;
 	root = cJSON_CreateObject();
+	format = cJSON_CreateObject();
 	cJSON_AddStringToObject(root, "name", "Jack (\"Bee\") Nimble");
-	cJSON_AddItemToObject(root, "format", fmt=cJSON_CreateObject());
-	cJSON_AddStringToObject(fmt,"type",		"rect");
-	cJSON_AddNumberToObject(fmt,"width",		1920);
-	cJSON_AddNumberToObject(fmt,"height",		1080);
-	cJSON_AddFalseToObject (fmt,"interlace");
-	cJSON_AddNumberToObject(fmt,"frame rate",	24);
+	cJSON_AddItemToObject(root, "format", format);
+	cJSON_AddStringToObject(format,"type","rect");
+	cJSON_AddNumberToObject(format,"width",1920);
+	cJSON_AddNumberToObject(format,"height",1080);
+	cJSON_AddFalseToObject (format,"interlace");
+	cJSON_AddNumberToObject(format,"frame rate",24);
 	//const char *my_json_string = cJSON_Print(root);
 	char *my_json_string = cJSON_Print(root);
 	ESP_LOGI(TAG, "my_json_string\n%s",my_json_string);
@@ -29,19 +30,19 @@ void app_main()
 	ESP_LOGI(TAG, "Deserialize.....");
 	cJSON *root2 = cJSON_Parse(my_json_string);
 	char *name = cJSON_GetObjectItem(root2,"name")->valuestring;
-	cJSON *fmt2 = cJSON_GetObjectItem(root2,"format");
-	char *type = cJSON_GetObjectItem(fmt2,"type")->valuestring;
-	int width = cJSON_GetObjectItem(fmt2,"width")->valueint;
-	int height = cJSON_GetObjectItem(fmt2,"height")->valueint;
-	bool interlace = cJSON_GetObjectItem(fmt2,"interlace")->valueint;
-	int frame_ratet = cJSON_GetObjectItem(fmt2,"frame rate")->valueint;
+	cJSON *format2 = cJSON_GetObjectItem(root2,"format");
+	char *type = cJSON_GetObjectItem(format2,"type")->valuestring;
+	int width = cJSON_GetObjectItem(format2,"width")->valueint;
+	int height = cJSON_GetObjectItem(format2,"height")->valueint;
+	bool interlace = cJSON_GetObjectItem(format2,"interlace")->valueint;
+	int frame_ratet = cJSON_GetObjectItem(format2,"frame rate")->valueint;
 	ESP_LOGI(TAG, "name=%s",name);
 	ESP_LOGI(TAG, "type=%s",type);
 	ESP_LOGI(TAG, "width=%d",width);
 	ESP_LOGI(TAG, "height=%d",height);
 	ESP_LOGI(TAG, "interlace=%d",interlace);
 	ESP_LOGI(TAG, "frame_ratet=%d",frame_ratet);
-	//cJSON_Delete(fmt2);
+	//cJSON_Delete(format2);
 	cJSON_Delete(root2);
 
 	// Buffers returned by cJSON_Print must be freed by the caller.
